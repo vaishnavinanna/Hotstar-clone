f666008309365f26a2212da7e47ae1c52893734d
Vaishnavi Nanna <90614283+vaishnavinanna@users.noreply.github.com>
2022-08-02 00:11:39 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,32 @@
+let movies = [
+  {
+    name: "falcon and the winter soldier",
+    des:
+      "Falcon and the Winter Soldier are a mismatched duo who team up for a global adventure that will test their survival skills -- as well as their patience!",
+    image: "slider 2.PNG"
+  },
+  {
+    name: "loki",
+    des:
+      "Loki, the God of Mischief, steps out of his brother's shadow to embark on an adventure that takes place after the events of Avengers: Endgame.!",
+    image: "slider 1.PNG"
+  },
+  {
+    name: "wanda vision",
+    des:
+      "Living idealized suburban lives, super-powered beings Wanda and Vision begin to suspect that everything is not as it seems!",
+    image: "slider 3.PNG"
+  },
+  {
+    name: "raya and the last dragon",
+    des:
+      "Raya, a warrior, sets out to track down Sisu, a dragon, who transferred all her powers into a magical gem which is now scattered all over the kingdom of Kumandra, dividing its people.!",
+    image: "slider 4.PNG"
+  },
+  {
+    name: "luca",
+    des:
+      "The movie follows Luca, Alberto, and Giulia's story of friendship and adventure in the Italian Riviera. Recently, Enrico Casarosa, director of Luca, revealed how his childhood inspired the movie. On Disney's official Twitter account, Enrico Casarosa shared how his story helped create Luca's story!!",
+    image: "slider 5.PNG"
+  }
+];
